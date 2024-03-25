062d120e1f2b4f9827a6eb7faf52e0ab0c626964
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-03-25 18:06:45 +0545

    We'r HERE

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const postController = require("./controller/PostController");
 const uploadFile = require("./controller/FileController");
 const mongoose = require("mongoose");
 const applicationController = require("./controller/applicationController");
+const env = require("dotenv");
 // const json = require("json()");
 
 app.use(express.json());
@@ -21,13 +22,12 @@ app.use("/application", applicationController);
 app.use(express.static("public"));
 app.use("/cv", express.static("cv"));
 
-mongoose.connect(
-  "mongodb+srv://siddharthasunuwar:H%40tepops00@cluster0.fjdontg.mongodb.net/Freelancing",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-app.listen(3333, () => {
+env.config();
+
+mongoose.connect(process.env.MONGO_USER, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
